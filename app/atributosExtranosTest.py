673d14b68c17799e90278e19351ee53bc4ff3f85
DESKTOP-OBJVK5C\INTEL <informacion.eafb@gmail.com>
2017-03-26 03:41:06 +0200

    se ajusta recursion, si encuentra un atributo extraño no sigue buscando

--- app/atributosExtranosTest.py
+++ app/atributosExtranosTest.py
@@ -141,6 +141,7 @@ def armar_cierre(list_a_cerrar, implicado, debug):
         if buscar_cadena(cadena1, cadena2, debug) != False:
             dic[str(str(cadena))] = True
             hay_extrano = True
+            break
         else:
             dic[str(str(cadena))] = False
         if debug == 1:
